451d8a6d02d7bb5174d3de5bc18985c35f0d0e15
areen <areenc04@gmail.com>
2023-02-16 01:00:24 +0530

    Initial Commit

---
+++ UCSDUnfoldingMaps/src/module1/Hello.java
@@ -0,0 +1,11 @@
+package module1;
+import java.util.*;
+import java.io.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		System.out.println("hcurv");
+	}
+}
